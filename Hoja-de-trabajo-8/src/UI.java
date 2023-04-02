b16058764f16cbfec55a91c006e6096a681bdf72
Jose-Prince <joseprince148mail.com>
2023-04-02 16:33:08 -0600

    Priorities queues
    Se crea la instancia para las implementaciones de las priorities queues

--- Hoja-de-trabajo-8/src/UI.java
+++ Hoja-de-trabajo-8/src/UI.java
@@ -1,5 +1,44 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class UI {
     public static void main(String[] args) throws Exception {
+        Scanner in = new Scanner(System.in);
+        boolean ejecucion = true;
+
+        priorityQ<Integer> queue = new priorityQ<Integer>();
+
+        while(ejecucion){
+            System.out.println("\nIndique la implementación del Heap a utilizar: " +
+            "\n1: Java Collections FrameWork" +
+            "\n2: Java Structures\n");
+            try {
+                int rpa = in.nextInt();
+                in.nextLine();
+
+                Object heap = queue.getInstance(rpa);
+                ejecucion = false;
+            } catch (Exception e) {
+                in.nextLine();
+                System.out.println("Debe elegir entre 1 o 2");
+                // TODO: handle exception
+            }
+        }
+
+        try {
+            File myFile = new File("procesos");
+            Scanner myReader = new Scanner(myFile);
+            while (myReader.hasNextLine()){
+                String data = myReader.nextLine();
+                System.out.println(data);
+            }
+
+            myReader.close();
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+
+
         
     }
 }
--- Hoja-de-trabajo-8/src/priorityQ.java
+++ Hoja-de-trabajo-8/src/priorityQ.java
@@ -2,10 +2,25 @@
  * The Author of this arbolbinario @Andre Jo
  */
 import java.util.PriorityQueue;
+import structure5.VectorHeap;
 
 
 
-public class priorityQ{
+
+public class priorityQ<E>{
+
+    public Object getInstance(int instance) {
+        Object priority = null;
+
+        if (instance == 1){
+            priority = new PriorityQueue<E>(); 
+        } else if (instance == 2){
+            priority = new VectorHeap<>();
+        }
+
+        return priority;
+
+    }
     
     
 }
